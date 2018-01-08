bc06db3c1d24bf884f9a99a07341cf3bf999a8cb
qzchenwl@gmail.com <qzchenwl>
2018-01-09 00:02:18 +0800

    fix

--- util.py
+++ util.py
@@ -1,8 +1,15 @@
 import PIL
 import numpy as np
-from keras.preprocessing.image import load_img, img_to_array, flip_axis
+from keras.preprocessing.image import load_img, img_to_array, array_to_img, flip_axis
 
 VGG_MEAN = [123.68, 116.78, 103.94]
+
+def list_images(file):
+    """
+    Get all the images and labels in file
+    """
+    with open(file, 'r') as f:
+        lines = f.readlines()
 
 def random_crop(img, size):
     """
@@ -12,7 +19,7 @@ def random_crop(img, size):
     """
 
     height, width, channels = img.shape
-    crop_h, crop_w = crop_size
+    crop_h, crop_w = size
 
     range_h = height - crop_h
     range_w = width -crop_w
@@ -32,7 +39,7 @@ def center_crop(img, size):
     """
 
     height, width, channels = img.shape
-    crop_h, crop_w = crop_size
+    crop_h, crop_w = size
 
     center_h, center_w = height // 2, width // 2
     half_h, half_w = crop_h // 2, crop_w // 2
@@ -43,8 +50,6 @@ def center_crop(img, size):
         :]
 
     return crop_img
-
-
 
 def random_flip_left_right(img):
     if np.random.random() < 0.5:
@@ -59,9 +64,9 @@ def parse_function(filename, label):
     img = load_img(filename)
 
     smallest_side = 256.0
-    scale = smallest_side / img.width if img.height > img.width else smallest_side / height
+    scale = smallest_side / img.width if img.height > img.width else smallest_side / img.height
 
-    resized_img = img.resize((img.width * scale, img.height * scale), PIL.Image.BILINEAR)
+    resized_img = img.resize((int(img.width * scale), int(img.height * scale)), PIL.Image.BILINEAR)
     img_array = img_to_array(resized_img)
 
     return img_array, label
@@ -72,7 +77,7 @@ def parse_function(filename, label):
 # (5) Substract the per color mean `VGG_MEAN`
 # Note: we don't normalize the data here, as VGG was trained without normalization
 def training_preprocess_vgg(img, label):
-    crop_img = random_crop(img, size)
+    crop_img = random_crop(img, (224, 224))
     flip_img = random_flip_left_right(crop_img)
 
     means = np.reshape(VGG_MEAN, [1, 1, 3])
@@ -81,7 +86,7 @@ def training_preprocess_vgg(img, label):
     return centered_img, label
 
 def training_preprocess_mobilenet(img, label):
-    crop_img = random_crop(img, size)
+    crop_img = random_crop(img, (224, 224))
     flip_img = random_flip_left_right(crop_img)
 
     normalized_img = (flip_img - 127.5) / 127.5
@@ -94,20 +99,18 @@ def training_preprocess_mobilenet(img, label):
 # (4) Substract the per color mean `VGG_MEAN`
 # Note: we don't normalize the data here, as VGG was trained without normalization
 def val_preprocess_vgg(img, label):
-    crop_img = center_crop(img)
+    crop_img = center_crop(img, (224, 224))
 
     means = np.reshape(VGG_MEAN, [1, 1, 3])
     centered_img = crop_img - means
 
     return centered_img
 
-
 def val_preprocess_mobilenet(img, label):
-    crop_img = center_crop(img)
+    crop_img = center_crop(img, (224, 224))
 
     normalized_img = (flip_img - 127.5) / 127.5
 
     return normalized_img, label
-
 
 
